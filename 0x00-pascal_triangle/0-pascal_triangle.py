9049fa1c4687339c0ec8e88b5d542cd932c5a32c
Hassan Cissey <hcissey0@gmail.com>
2024-01-04 14:01:20 +0000

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,21 @@
 """Pascal Triangle"""
 
 def pascal_triangle(n):
+    """This is the pascal triangle generator"""
+    res = []
     if n <= 0:
-        return []
-    
+        return res
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if i == 0:
+                row.append(1)
+            elif i == 1:
+                row.append(1)
+            else:
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(res[i-1][j-1] + res[i-1][j])
+        res.append(row)
+    return res

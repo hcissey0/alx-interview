111247d52178166a0b4608e0463e488f1b8e383d
Hassan Cissey <hcissey0@gmail.com>
2024-01-10 04:36:57 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,6 +5,8 @@
 def canUnlockAll(boxes):
     """This function determines if all boxes can be unlocked"""
 
+    if boxes == None:
+        return False
     keys = [0,] + [j for i in boxes for j in i]
     for i, v in enumerate(boxes):
         if i in keys:

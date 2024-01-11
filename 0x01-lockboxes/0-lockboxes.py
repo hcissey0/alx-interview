8710f83366f6f955e1bb2fd81a20b07fe7b43c92
Hassan Cissey <hcissey0@gmail.com>
2024-01-11 06:40:01 +0000

    Lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,19 +5,27 @@
 def canUnlockAll(boxes):
     """This function determines if all boxes can be unlocked"""
 
-    if boxes is None:
-        return False
-    if type(boxes) != list:
-        return False
-    for i in boxes:
-        if type(i) != list:
-            return False
-    keys = [0,] + [j for i in boxes for j in i]
-    for i, v in enumerate(boxes):
-        if i not in keys:
-            return False
-        if i != 0:
-            if i in v and keys.count(i) == 1:
-                return False
+    # if boxes is None:
+    #     return False
+    # if type(boxes) != list:
+    #     return False
+    # for i in boxes:
+    #     if type(i) != list:
+    #         return False
+    # keys = [0,] + [j for i in boxes for j in i]
+    # for i, v in enumerate(boxes):
+    #     if i not in keys:
+    #         return False
+    #     if i != 0:
+    #         if i in v and keys.count(i) == 1:
+    #             return False
 
+    # return True
+
+    keys = [0]
+    for i, box in enumerate(boxes):
+        if i in keys:
+            keys.extend(box)
+        else:
+            return False
     return True

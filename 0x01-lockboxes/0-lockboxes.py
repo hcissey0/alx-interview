5f501e1e9681cb83d537e9c3ed54f44a6adeda03
Hassan Cissey <hcissey0@gmail.com>
2024-01-11 07:03:57 +0000

    Lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -22,16 +22,30 @@ def canUnlockAll(boxes):
 
     # return True
 
-    i = 0
-    total = list(set(boxes[0]) | {0})
-    while i < len(total):
-        newkeys = boxes[total[i]]
-        total += [key for key in newkeys if key not in total]
-        i += 1
-    return len(total) == len(boxes)
+    # i = 0
+    # total = list(set(boxes[0]) | {0})
+    # while i < len(total):
+    #     newkeys = boxes[total[i]]
+    #     total += [key for key in newkeys if key not in total]
+    #     i += 1
+    # return len(total) == len(boxes)
+    
     # keys = [0]
     # for i, box in enumerate(boxes):
     #     if i != 0 and i in box and keys.count(i) == 0:
     #         return False
     #     keys.extend(box)
     # return True
+
+    total_boxes = len(boxes)
+    unlocked = [False] * total_boxes
+    unlocked[0] = True
+    keys = [key for key in boxes [0] if key < total_boxes]
+    while keys:
+        new_keys = []
+        for key in keys:
+            if not unlocked[key]:
+                unlocked[key] = True
+                new_keys += [new_key for new_key in boxes[key] if new_key < total_boxes]
+                keys = new_keys
+    return all(unlocked)

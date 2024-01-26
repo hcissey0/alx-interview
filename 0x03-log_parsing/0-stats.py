d579e159c987b818160432375d217266c18c2790
Hassan Cissey <hcissey0@gmail.com>
2024-01-25 21:48:12 -0800

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,41 @@
-#!/usr/bin/python3
-"""This is the stat file"""
+#!/usr/bin/env python3
 
 import sys
 
 
-total_size = 0
-status_codes = {
-        str(i): 0
-        for i in [200, 301, 400, 401, 403, 404, 405, 500]
-        }
-line_count = 0
-
-def print_stats():
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
-
+t_size = 0
+s_codes = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
+l_count = 0
 
 try:
     for line in sys.stdin:
-        line_count += 1
         try:
-            parts = line.split()
-            file_size = int(parts[-1])
-            total_size += file_size
-            code = int(parts[-2])
-            status_codes[code] += 1
+            args = line.split()
+            size = int(args[-1])
+            code = int(args[-2])
+
+            if code in s_codes:
+                s_codes[code] += 1
+
+            t_size += size
+            l_count += 1
+
+            if l_count % 10 == 0:
+                print(f"File size: {t_size}")
+                [print(f"{c}: {v}") for c, v in s_codes.items() if v != 0]
         except Exception:
             pass
-        if line_count % 10 == 0:
-            print(f"File size: {total_size}")
-            for c in sorted(status_codes):
-                print(f"{c}: {status_codes[c]}")
-
 except KeyboardInterrupt:
-    print(f"File size: {total_size}")
-    for c in sorted(status_codes):
-        print(f"{c}: {status_codes[c]}")
+    pass
+finally:
+    print(f"File size: {t_size}")
+    [print(f"{c}: {v}") for c, v in s_codes.items() if v != 0]
